18022cd1903223b14e613138631eee25d9cb7cb6
SliiZ-code <101480069+SliiZ-code@users.noreply.github.com>
2022-10-17 22:02:40 +0200

    fonctionnel

--- scraper.py
+++ scraper.py
@@ -4,7 +4,6 @@ import pandas
 from datetime import date
 from tkinter import *
 
-date = date.today()
 url = "https://www.amazon.fr/Logitech-Volant-Course-Cuir-m%C3%A9tal/dp/B00YUIM2J0/?_encoding=UTF8&pd_rd_w=Pu4gD&content-id=amzn1.sym.d01040f0-2953-47b0-8f72-922a5679b397&pf_rd_p=d01040f0-2953-47b0-8f72-922a5679b397&pf_rd_r=03FBKY0FD1VGJXXRF4Q4&pd_rd_wg=1sx4N&pd_rd_r=cb3e0c90-96f1-47b1-a61a-a19f937e4e8f&ref_=pd_gw_ci_mcx_mr_hp_atf_m"
 
 headers = {
@@ -13,19 +12,36 @@ headers = {
 page = requests.get(url, headers=headers)
 
 
+def recup_donnees():
+    global liste_dates
+    global liste_prix
+    tableau = pandas.read_csv("Prices.csv")
+    tableautrie = tableau.sort_values(by=["Date"], ascending=False)
+    tableau = tableautrie.reset_index(drop=True)
+    liste = tableau.iloc[:5]
+    liste_dates = liste['Date'].values.tolist()
+    liste_prix = liste['Prix'].values.tolist()
+
+
+recup_donnees()
+
+
 def scrap():
     if page.ok:
         soup = BeautifulSoup(page.text, 'html.parser')
         prices = soup.find(class_="a-offscreen")
         prix = prices.text
-
-        tableau = {"Date": [date], "Prix": [prix]}
+        datedujour = date.today()
+        tableau = {"Date": [datedujour], "Prix": [prix]}
         df = pandas.DataFrame.from_dict(tableau)
         df.to_csv("Prices.csv", mode='a', header=False, index=False)
 
 
 def refresh():
+    recup_donnees()
+    listes()
     window.update()
+    window.after(100, refresh)
 
 
 window = Tk()
@@ -47,5 +63,20 @@ button.pack(pady=25)
 button = Button(frame, text="Rafraichir", bg="#00B8FC",
                 fg="#1c1c1c", command=refresh)
 button.pack(pady=40)
+
+
+def listes():
+    listebox_prix = Listbox(window, bg="#333333", fg="WHITE", font="BOLD")
+    for i in range(len(liste_prix)):
+        listebox_prix.insert(i, liste_prix[i])
+    listebox_prix.place(x=65, y=350)
+    listebox_dates = Listbox(window, bg="#333333", fg="WHITE", font="BOLD")
+    for i in range(len(liste_dates)):
+        listebox_dates.insert(i, liste_dates[i])
+    listebox_dates.place(x=250, y=350)
+
+
+listes()
+refresh()
 
 window.mainloop()
